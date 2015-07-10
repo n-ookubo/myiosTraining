187721c6888e3074a7221fd5247859c025f40cc8
n-ookubo <ohkubo.n.ab@gmail.com>
2015-07-10 16:16:23 +0900

    Initial Commit

---
+++ 3.2/CustomUIViewSample/CustomUIViewSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomUIViewSample
+//
+//  Created by 大久保直昭 on 2015/07/10.
+//  Copyright (c) 2015年 大久保直昭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
