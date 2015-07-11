6efd059bd8adcf336229ccddc20c2bf1ffad35a7
n-ookubo <ohkubo.n.ab@gmail.com>
2015-07-11 18:47:19 +0900

    4.1 4.2

--- 4.1/TableViewSample/TableViewSample/ViewController.m
+++ 4.1/TableViewSample/TableViewSample/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -17,11 +18,50 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return section + 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+        //[self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
+        //cell = [self.tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld", indexPath.row + 1];
+    return cell;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 10;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    return [NSString stringWithFormat:@"section %ld", section + 1];
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    NSString *mes = [NSString stringWithFormat:@"section %ld, row %ld", indexPath.section + 1, indexPath.row + 1];
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"UITableView" message:mes preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+    [self.view.window.rootViewController presentViewController:alert animated:YES completion:nil];
 }
 
 @end
