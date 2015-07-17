885d61d9fbc281853d71875d8335136af86d360f
n-ookubo <ohkubo.n.ab@gmail.com>
2015-07-17 14:11:04 +0900

    Initial Commit

---
+++ 6.1/HttpRequestSample/HttpRequestSampleTests/HttpRequestSampleTests.m
@@ -0,0 +1,40 @@
+//
+//  HttpRequestSampleTests.m
+//  HttpRequestSampleTests
+//
+//  Created by 大久保直昭 on 2015/07/17.
+//  Copyright (c) 2015年 大久保直昭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface HttpRequestSampleTests : XCTestCase
+
+@end
+
+@implementation HttpRequestSampleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
