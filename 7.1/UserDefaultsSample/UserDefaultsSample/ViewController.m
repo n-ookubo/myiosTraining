4367a99b0bb4dbcea8469a819f12d86977968e17
n-ookubo <ohkubo.n.ab@gmail.com>
2015-07-21 10:39:07 +0900

    Initial Commit

---
+++ 7.1/UserDefaultsSample/UserDefaultsSample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UserDefaultsSample
+//
+//  Created by 大久保直昭 on 2015/07/21.
+//  Copyright (c) 2015年 大久保直昭. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
